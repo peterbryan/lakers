a601f9312c10879837113b5aa5b25ae40c8efbc3
Peter <peterbryanr@gmail.com>
2013-06-29 10:58:53 -0700

    First Commit

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,5 +1,50 @@
-# $('./body') {
-#   insert_top("header", class: "_header") {
-#     Move stuff here
-#   }
-# }
+$('./body') {
+  $(".//div[@id='header']") {
+      # Move top menu into #Logo container
+      $("./div[@id='logo']") {
+      move_here("../../../div[@id='header']", "bottom"){
+        $("./ul") {
+          # Remove unwanted top menu items from upper menu
+          $("./li[position() = 1 or position() > 2 and not(position()=last()-1)]") {
+            remove()
+          }     
+        }
+      }
+
+      # Inject header template
+      inject_after(read("../sections/header.html"))
+
+      # Fill search template
+      $("..//div[@class='mw_search']"){
+        # Move search form into container element
+        move_here("//div[@id='block-search-0']", "bottom"){
+          $("./p") {
+            remove()
+          }
+          $("./form") {
+            $("./label") {
+              remove()
+          }					 
+          
+            # Change search button background image
+            $("./input[@type='image']") {
+              wrap("div", class: "mw_search_btn buttons-search")
+            }
+
+            $("./input[@type='form.text']") {
+              attribute("value", "Search…")
+            } 
+
+          }     
+        }
+      }   
+    }                       
+    # Remove br
+    $("./br") {
+      remove()
+    }
+
+    # Temporarily remove header links
+    remove("../div[@id='Menu']")
+  }
+}
